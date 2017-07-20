36a077677c33ba57dd0e8765437a646d7e6a8024
Victor Allen <vamuigua@gmail.com>
2017-07-20 17:15:29 +0300

    Updated route for teams

--- app.rb
+++ app.rb
@@ -7,19 +7,42 @@ require "./lib/team"
 require('pg')
 require('pry')
 
-#######INDEX ROUTE########
+####### INDEX ROUTE ########
 get('/') do
   @teams = Team.all()
   erb(:index)
 end
 
-######ADD TEAM ROUTE#######
+###### ADD TEAM ROUTE #######
 get('/teams/new') do
   erb(:teams_form)
 end
 
-######TEAMS ROUTE#######
+###### TEAMS ROUTE #######
 get('/teams') do
+  @teams = Team.all()
+  erb(:teams)
+end
+
+######## POST REQUEST FOR TEAMS############
+post('/teams') do
+  name = params.fetch("name")
+  new_team = Team.create({:name => name,:id => nil})
+  @teams = Team.all()
+  erb(:teams)
+end
+
+###### EDIT/UPDATE TEAM INFO ##############
+get('/teams/:id/edit') do
+  @team = Team.find(params.fetch("id").to_i())
+  erb(:team_edit)
+end
+
+######## TO PATCH/UPDATE TEAM DETAILS ##########
+patch("/teams/:id") do
+  name = params.fetch("name")
+  @team = Team.find(params.fetch("id").to_i())
+  @team.update({:name => name})
   @teams = Team.all()
   erb(:teams)
 end
