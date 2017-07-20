fce59099e245a5bd36018a00ec64c2d8a0549daa
Victor Allen <vamuigua@gmail.com>
2017-07-20 15:52:13 +0300

    Modified spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,18 @@
 #require the same files and gems
 require("rspec")
 require("pg")
+require("sinatra/activerecord")
 require("team")
 require("player")
-require("tournament")
 
-#create a database connection
-DB = PG.connect({:dbname => "team_league_test"})
-
-#set up RSpec to clean the database between test runs
+#configuration to DB
 RSpec.configure do |config|
   config.after(:each) do
-    DB.exec("DELETE FROM teams *;")
-    DB.exec("DELETE FROM players *;")
-    DB.exec("DELETE FROM tournaments *;")
+    Team.all().each() do |team|
+      team.destroy()
+    end
+    Player.all.each() do |player|
+      player.destroy()
+    end
   end
 end
