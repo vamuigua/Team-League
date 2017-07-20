39cc582f557eabba56928d25b233824180f2c45a
Victor Allen <vamuigua@gmail.com>
2017-07-20 14:52:06 +0300

    Added js for the nav, parallax and scrollspy

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+    //Function for navbar
     $(".button-collapse").sideNav();
+    //Function for parallax in images
     $('.parallax').parallax();
+    //Function for scrollspy
+    $('.scrollspy').scrollSpy();
 })
